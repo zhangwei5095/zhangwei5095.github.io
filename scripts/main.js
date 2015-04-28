e82ec88eee8653b53d395a8ee27da8ce8779657c
waylau <waylau521@gmail.com>
2015-04-28 20:59:41 +0800

    update  to  theme  jekyll-bootstrap-blog

---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+$(function(){$(window).scroll(function(){var a=document.documentElement.scrollTop+document.body.scrollTop;if(a>200){$("#goToTop").fadeIn(400)}else{$("#goToTop").stop().fadeOut(400)}});$("#goToTop").click(function(){$("html,body").animate({scrollTop:"0px"},200)})});
